1e1a009081a4b4ff2af258b3db1fad6874751f57
Guerrero1232 <Igo2rlol@gmail.com>
2024-03-09 22:11:11 +0300

    added servise package

--- src/main/java/org/example/controller/Controller.java
+++ src/main/java/org/example/controller/Controller.java
@@ -1,6 +1,7 @@
 package org.example.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api")
 public class Controller {
 
+    @PostMapping
+    private void getOrder(){
+
+    }
 }
