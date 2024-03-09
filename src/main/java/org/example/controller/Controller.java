f81321682509841f02a110351a889a38cd84f584
Guerrero1232 <Igo2rlol@gmail.com>
2024-03-09 21:54:06 +0300

    preparation of the working environment: Spring environment setup Docker container setup PostgreSQL database setup

---
+++ src/main/java/org/example/controller/Controller.java
@@ -0,0 +1,12 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("api")
+public class Controller {
+
+}
---
+++ src/main/java/org/example/storage/Order.java
@@ -0,0 +1,22 @@
+package org.example.storage;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+}
