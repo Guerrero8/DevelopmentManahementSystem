dd2a494e86f89e9879f119afee74c9039a41577c
Guerrero1232 <Igo2rlol@gmail.com>
2024-03-18 09:57:00 +0300

    added order structure

--- src/main/java/org/example/storage/Order.java
+++ src/main/java/org/example/storage/Order.java
@@ -1,22 +1,32 @@
 package org.example.storage;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 
 @Entity
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
 @NoArgsConstructor
 public class Order {
+    {
+        //todo сделать сортировку по дедлайну и статусу заказа(фраза "заказ нуждается в срочном выполнении либо в продлении срока действия договора")
+    }
     @Id
     @GeneratedValue
-    private Integer id;
-
+    private Integer orderId; // id заказа
+    private String clientSurname; // Фамилия клиента сделавшего этот заказ
+    private String orderStatus; // статус выполнения заказа
+    private String address; // адрес проведения работ
+    private LocalDate dateOfContractConclusion; // дата заключения договора
+    private LocalDateTime dateTimeOfInstallation; // назначенная дата выполнения заказа
+    private LocalDate DeadlineForServiceProvision; // крайний срок выполнения заказа
+    private float orderAmount; // цена работ
 }
